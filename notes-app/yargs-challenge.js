a7523f93358caca10e4d2f7b912a2f1028fa294d
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-06-21 06:32:58 -0300

    notes-app: using yargs to make node commands - part 2

--- notes-app/yargs-challenge.js
+++ notes-app/yargs-challenge.js
@@ -1,35 +1,51 @@
+//Yargs helps you build interactive command line tools, by parsing arguments and generating an elegant user interface.
 const yargs = require('yargs');
 
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note!',
-    handler: function() {
-        console.log('Adding a new note');
-    }
+  command: 'add',
+  describe: 'Add a new note!',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function(argv) {
+    console.log('Title:', argv.title);
+    console.log('Body:', argv.body);
+  }
 });
 
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note!',
-    handler: function() {
-        console.log('Removing the note!');
-    }
+  command: 'remove',
+  describe: 'Remove a note!',
+  handler: function() {
+    console.log('Removing the note!');
+  }
 });
 
 yargs.command({
-    command: 'read',
-    describe: 'Read a note!',
-    handler: function() {
-        console.log('Reading the note');
-    }
+  command: 'read',
+  describe: 'Read a note!',
+  handler: function() {
+    console.log('Reading the note');
+  }
 });
 
 yargs.command({
-    command: 'list',
-    describe: 'List your notes!',
-    handler: function() {
-        console.log('Listing out all notes!');
-    }
+  command: 'list',
+  describe: 'List your notes!',
+  handler: function() {
+    console.log('Listing out all notes!');
+  }
 });
 
-console.log(yargs.argv);
+// Faz o parse dos comandos a serem executados e mostra no terminal
+yargs.parse();
+//console.log(yargs.argv);
