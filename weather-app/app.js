5776ff3ec65c5aaba26f7381a444af904379985c
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-06-27 08:52:41 -0300

    weather-app: an HTTP request challeng - mapbox api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,19 @@
 // The request lib is deprecated but we have postman-request which is a fork of the original
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=6200ec9ce5a600351af24de5cbe75093&query=37.8267,-122.4233';
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianVsaWFub3BhZGlsaGEiLCJhIjoiY2p1NDV3OTl4MHVsYTQzbnpjN2I0bDdhbCJ9.gQPOKPnC6QQTEWTDaLJnAg&limit=1';
 
-request({ url, json: true }, (error, response) => {
+const weatherUrl = 'http://api.weatherstack.com/current?access_key=6200ec9ce5a600351af24de5cbe75093&query=37.8267,-122.4233';
+
+request({ url: weatherUrl, json: true }, (error, response) => {
   const { current } = response.body;
   const { temperature, feelslike, weather_descriptions } = current;
   console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`);
+});
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const { features } = response.body;
+  const { center } = features[0];
+  const [longitude, latitude] = center;
+  console.log(`${latitude}, ${longitude}`);
 });
