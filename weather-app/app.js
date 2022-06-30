63fcf0d2ff01185025b9219604c15bc1e2af8523
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-06-29 23:02:34 -0300

    weather-app: callback abstraction challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,12 @@
-// The request lib is deprecated but we have postman-request which is a fork of the original
-const request = require('postman-request');
+const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
-
-// const weatherUrl = 'http://api.weatherstack.com/current?access_key=6200ec9ce5a600351af24de5cbe75093&query=37.8267,-122.4233';
-
-// request({ url: weatherUrl, json: true }, (error, response) => {
-//   if (error) {
-//     console.log('Unable to connect to weather service!');
-//   } else if (response.body.error) {
-//     console.log('Unable to find location!');
-//   } else {
-//     const { current } = response.body;
-//     const { temperature, feelslike, weather_descriptions } = current;
-//     console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`);
-//   }
-// });
 
 geocode('Santo André', (error, data) => {
   console.log(error);
   console.log(data);
+});
+
+forecast(44.1545, -75.7088, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 });
