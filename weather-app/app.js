0479d106551b35d40e6d73ccc0ca73b0cb1fff49
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-07-04 08:26:19 -0300

    weather-app: destructuring property shorthand challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,15 +6,15 @@ const addressLocationInputFromTeminal = process.argv[2];
 if (!addressLocationInputFromTeminal) {
   return console.log('Please provide an address.');
 }
-geocode(addressLocationInputFromTeminal, (error, geocodeData) => {
+geocode(addressLocationInputFromTeminal, (error, { latitude, longitude, location } = {}) => {
   if (error) {
     return console.log(error);
   }
-  forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+  forecast(latitude, longitude, (error, forecastData) => {
     if (error) {
       return console.log(error);
     }
-    console.log(geocodeData.location);
+    console.log(location);
     console.log(forecastData);
   });
 });
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -9,13 +9,13 @@ const request = require('postman-request');
   const token = 'pk.eyJ1IjoianVsaWFub3BhZGlsaGEiLCJhIjoiY2p1NDV3OTl4MHVsYTQzbnpjN2I0bDdhbCJ9.gQPOKPnC6QQTEWTDaLJnAg';
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${token}&limit=1`;
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined);
-    } else if (!response.body.features.length) {
+    } else if (!body.features.length) {
       callback('Unable to find location. Try another search.', undefined);
     } else {
-      const { features } = response?.body;
+      const { features } = body;
       const { center, place_name } = features[0];
       const [longitude, latitude] = center;
       callback(undefined, {
