0b4813d96f3e3a28d9719c7a75eb276d5719d4b8
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-07-05 07:27:54 -0300

    web-server: serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,17 +2,26 @@ const express = require('express');
 
 const app = express();
 
+// app.get('', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 app.get('', (req, res) => {
-  res.send('Hello Express');
+  res.send('<h1>Home</h1>');
 });
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send({
+    name: 'Juliano',
+    age: 30
+  });
 });
 app.get('/about', (req, res) => {
-  res.send('About page')
+  res.send('<h1>About page<h1>');
 });
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: 'It is snowing!',
+    location: 'New York'
+  });
 });
 
 app.listen(3000, () => {
