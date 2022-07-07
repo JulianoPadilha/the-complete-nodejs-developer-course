4b25107b04d072368a0356e34066f984c4ea22fd
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-07-06 21:26:21 -0300

    web-server: dynamic pages with templating using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,26 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Juliano Padilha',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Juliano Padilha',
+  });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is the help page!',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
