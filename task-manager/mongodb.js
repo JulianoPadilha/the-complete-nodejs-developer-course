f57fe08c74c330f770ab3fbfd54e89b99050bb1d
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-07-18 07:02:49 -0300

    task-manager: inserting documents - insertOne and insertMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,8 +10,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     return console.log('Unable to connect to database!');
   }
   const db = client.db(databaseName);
-  db.collection('users').insertOne({
-    name: 'Juliano',
-    age: 29,
+  // db.collection('users').insertOne({
+  //   name: 'Juliano',
+  //   age: 29,
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user');
+  //   }
+
+  //   console.log(result);
+  // });
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Juliano',
+  //     age: 29
+  //   },
+  //   {
+  //     name: 'Carol',
+  //     age: 33
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert users!');
+  //   }
+
+  //   console.log(result.insertedIds);
+  // });
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Task 1',
+      completed: true
+    },
+    {
+      description: 'Task 2',
+      completed: true
+    },
+    {
+      description: 'Task 3',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert tasks!');
+    }
+
+    console.log(result.insertedIds);
   });
 });
