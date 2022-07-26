0912187a49a3685ed4c82b15f0c62d6ed7c42a80
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-07-26 06:45:05 -0300

    task-manager: mongodb - the ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,9 +1,19 @@
-const mongodb = require('mongodb');
-
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+/* 
+https://www.mongodb.com/docs/manual/reference/method/ObjectId/
+Returns a new ObjectId. The 12-byte ObjectId consists of:
+
+- A 4-byte timestamp, representing the ObjectId's creation, measured in seconds since the Unix epoch.
+- A 5-byte random value generated once per process. This random value is unique to the machine and process.
+- A 3-byte incrementing counter, initialized to a random value.
+*/
+// const id = new ObjectId();
+// console.log(id);
+// console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -38,24 +48,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   //   console.log(result.insertedIds);
   // });
 
-  db.collection('tasks').insertMany([
-    {
-      description: 'Task 1',
-      completed: true
-    },
-    {
-      description: 'Task 2',
-      completed: true
-    },
-    {
-      description: 'Task 3',
-      completed: false
-    }
-  ], (error, result) => {
-    if (error) {
-      return console.log('Unable to insert tasks!');
-    }
+  // db.collection('tasks').insertMany([
+  //   {
+  //     description: 'Task 1',
+  //     completed: true
+  //   },
+  //   {
+  //     description: 'Task 2',
+  //     completed: true
+  //   },
+  //   {
+  //     description: 'Task 3',
+  //     completed: false
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert tasks!');
+  //   }
 
-    console.log(result.insertedIds);
-  });
+  //   console.log(result.insertedIds);
+  // });
 });
