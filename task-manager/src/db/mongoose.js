5b5eecc8328971070db845f42a0ff90f3b0e04ef
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-08-13 15:04:20 -0300

    task-manager: data validation and sanitization: part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,44 +1,64 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
 const User = mongoose.model('User', {
   name: {
     type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
   },
 });
 
-// const me = new User({
-//   name: 'Juliano',
-//   age: 29
-// });
-
-// me.save().then(() => {
-//   console.log(me);
-// }).catch((error) => {
-//   console.log(error);
-// });
-
-// === Challenge ===//
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
+const me = new User({
+  name: '  Juliano  ',
+  email: 'TESTE@teste.com  ',
 });
 
-const task = new Task({
-  description: 'Task 1',
-  completed: false,
-});
-
-task.save().then(() => {
-  console.log(task);
+me.save().then(() => {
+  console.log(me);
 }).catch((error) => {
   console.log(error);
 });
+
+// === Challenge ===//
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// const task = new Task({
+//   description: 'Task 1',
+//   completed: false,
+// });
+
+// task.save().then(() => {
+//   console.log(task);
+// }).catch((error) => {
+//   console.log(error);
+// });
