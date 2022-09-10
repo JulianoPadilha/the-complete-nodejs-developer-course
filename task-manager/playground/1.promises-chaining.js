262cb403f0ff4e98610ca83a327c37a5d9d2b7f4
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-09-09 21:56:15 -0300

    task-manager: playground - async/await challenge

--- task-manager/playground/1.promises-chaining.js
+++ task-manager/playground/1.promises-chaining.js
@@ -2,11 +2,22 @@ require('../src/db/mongoose');
 const User = require('../src/models/user');
 
 // Seleciona um usuário e atualiza sua idade, depois encadeia um outra promise para retornar a quantidade de usuário com a idade da query
-User.findByIdAndUpdate('62fa747946034451a103020b', { age: 1 }).then(user => {
-  console.log(user);
-  return User.countDocuments({ age: 1 });
-}).then(result => {
-  console.log(result);
-}).catch(error => {
-  console.log(error);
-});
+// User.findByIdAndUpdate('62fa747946034451a103020b', { age: 1 }).then(user => {
+//   console.log(user);
+//   return User.countDocuments({ age: 1 });
+// }).then(result => {
+//   console.log(result);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+// Challenge usando async/await
+const updateAgeAndCount = async (userId, age) => {
+  await User.findByIdAndUpdate(userId, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('62fa747946034451a103020b', 2)
+  .then(count => console.log(count))
+  .catch(error => console.log(error));
--- task-manager/playground/2.promises-chaining.js
+++ task-manager/playground/2.promises-chaining.js
@@ -1,11 +1,21 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('62e8f534715e0558eebb1050').then(task => {
-  console.log(task);
-  return Task.countDocuments({ completed: false });
-}).then(result => {
-  console.log(result);
-}).catch(error => {
-  console.log(error);
-})
+// Task.findByIdAndDelete('62e8f534715e0558eebb1050').then(task => {
+//   console.log(task);
+//   return Task.countDocuments({ completed: false });
+// }).then(result => {
+//   console.log(result);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+const deleteTaskAndCount = async (taskId) => {
+  await Task.findByIdAndDelete(taskId);
+  const count = Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount('62fee4d624de5848055d2494')
+  .then(count => console.log(count))
+  .catch(error => console.log(error));
