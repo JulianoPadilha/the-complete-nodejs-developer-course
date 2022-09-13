3e2e0fc253f87db18595c3e31761e30ebd21269e
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-09-12 22:03:17 -0300

    task-manager: securely storing passwords

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -43,7 +43,12 @@ router.patch('/tasks/:id', async (req, res) => {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    const task = await Task.findById(req.params.id);
+    updates.forEach(update => task[update] = req.body[update]);
+    task.save();
+
+    // Mudança feita para suportar o uso de middlewares do mongoose
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
     if (!task) {
       return res.status(404).send();
     }
