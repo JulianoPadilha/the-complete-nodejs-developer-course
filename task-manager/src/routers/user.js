bc34b6a05597aa3dd8b92b9672b5007a098a56a5
Juliano Padilha <juliano.padilha@mercadolivre.com>
2022-09-23 22:24:31 -0300

    task-manager: authenticating user endpoints - 113

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -50,21 +50,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return rest.status(404).send();
-    }
-    res.send(user);
-  } catch(error) {
-    // Se for um id inválido (menos ou mais do que 12 caracteres) vai cair no catch
-    res.status(500).send(error);
-  }
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'password', 'email', 'age'];
   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -72,28 +58,18 @@ router.patch('/users/:id', async (req, res) => {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
   try {
-    const user = await User.findById(req.params.id);
-    updates.forEach(update => user[update] = req.body[update]);
-    await user.save();
-
-    // Mudança feita para suportar o uso de middlewares do mongoose
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    updates.forEach(update => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
   } catch(error) {
     res.status(400).send(error);
   }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch(error) {
     res.status(400).send(error);
   }
